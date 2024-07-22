2f266a1e2d0fa548c2470ea4388629cc30209fe9
bayaraa <bayartsogt333@gmail.com>
2024-07-22 15:01:09 +0800

    finance modul ruu add hiideg bolow

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ var financeController = (function () {
     }
     //4 ogogdl damjuuljihar neg objectd hiigd ogcvvl zvgeer. 
     var data = {
-        allItems: {
+        items: {
             inc: [],
             exp: []
         },
@@ -46,16 +46,37 @@ var financeController = (function () {
             exp: 500
         }
     }
+
+    return {
+        addItem: function (type, description, val) {
+            var item, id;
+            if (data.items[type].length == 0)
+                id = 1;
+            else
+                id = data.items[type][data.items[type].length - 1].id + 1;
+            //idg hamgin ar deeer + 1 gd generate hiijin.
+
+            if (type == 'inc') {
+                item = new Income(id, description, val);
+            } else {
+                item = new Expense(id, description, val);
+            }
+            data.items[type].push(item);
+        },
+        getData: function () {
+            return data;
+        }
+    }
 })();
 
 // programmin holbogc controller
 var appController = (function (uiController, financeControoller) {
     function addItem() {
         // 1. oruulah ogogdliig delgetsees olj awna. 
-        console.log(uiController.getInput());
-
+        var input = uiController.getInput();
 
         // 2. olj awsan ogogdlvvde sanhvvgin controllert damjuulj tend hadgalna.s
+        financeController.addItem(input.type, input.description, input.value);
 
         // 3. olj awsan ogogdlvvde tohiroh hesegt n gargana.
 
